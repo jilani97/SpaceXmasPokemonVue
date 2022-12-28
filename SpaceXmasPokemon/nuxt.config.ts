06160d49224fecdaeb5fbe9c3029b7d879a5cffb
ilji <ilham.jillani@netlight.com>
2022-12-28 18:37:17 +0100

    Tailwind first page try

--- SpaceXmasPokemon/nuxt.config.ts
+++ SpaceXmasPokemon/nuxt.config.ts
@@ -1,4 +1,22 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  modules: ["@nuxtjs/tailwindcss"],
+  app: {
+    pageTransition: { name: "page", mode: "in-out" },
+    head: {
+      charset: "utf-16",
+      viewport: "width=device-width, initial-scale=1",
+      title: "SpaceXmasPokemon",
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://design.swedbankpay.com/v/10.5.0/styles/swedbankpay.css",
+        },
+        { rel: "stylesheet", href: "/css/global.css" },
+      ],
+      meta: [{ name: "description", content: "My SpaceXmas Pokemon Project." }],
+    },
+  },
+  tailwindcss: {
+    cssPath: "~/assets/style/variables.scss",
+  },
+});
